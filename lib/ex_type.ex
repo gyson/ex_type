cce62cdfdc8c8808651f2aed84425821ce633cbd
gyson <eilian.yunsong@gmail.com>
2019-02-24 10:42:27 -0500

    Init mix

---
+++ lib/ex_type.ex
@@ -0,0 +1,18 @@
+defmodule ExType do
+  @moduledoc """
+  Documentation for ExType.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExType.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_type_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTypeTest do
+  use ExUnit.Case
+  doctest ExType
+
+  test "greets the world" do
+    assert ExType.hello() == :world
+  end
+end
