e5a6d79250312a6238388345f45de5d6cc41e812
gyson <eilian.yunsong@gmail.com>
2019-04-22 16:45:02 -0400

    Update typespec for MapSet

--- lib/ex_type/typespec/elixir/map_set.ex
+++ lib/ex_type/typespec/elixir/map_set.ex
@@ -1,11 +1,9 @@
 import ExType.Typespec, only: [deftypespec: 2]
 
 deftypespec MapSet do
-  @type t(x) :: T.opaque(x)
+  @spec new() :: MapSet.t(any())
 
-  @spec new() :: t(any())
+  @spec new(T.p(Enumerable, x)) :: MapSet.t(x) when x: any()
 
-  @spec new(T.p(Enumerable.t(), a)) :: t(a) when a: any()
-
-  @spec new(T.p(Enumerable.t(), a), (a -> b)) :: t(b) when a: any(), b: any()
+  @spec new(T.p(Enumerable, a), (a -> b)) :: MapSet.t(b) when a: any(), b: any()
 end
