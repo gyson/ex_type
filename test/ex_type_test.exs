dec2f8d617603d33786d85182907980949bfe7fc
Manuel Bärenz <programming@manuelbaerenz.de>
2022-04-01 18:35:56 +0200

    Print IO when tests fail

--- test/ex_type_test.exs
+++ test/ex_type_test.exs
@@ -13,16 +13,18 @@ defmodule ExTypeTest do
   for file <- Path.wildcard("#{__DIR__}/type_checks/**/*.ex") do
     test "type check test case #{file}" do
       result = capture_io(fn -> ExType.check(unquote(file)) end)
-      assert String.contains?(result, [Emoji.one_test_pass()])
-      refute String.contains?(result, [Emoji.one_test_fail()])
+
+      assert String.contains?(result, [Emoji.one_test_pass()]), result
+      refute String.contains?(result, [Emoji.one_test_fail()]), result
     end
   end
 
   for file <- Path.wildcard("#{__DIR__}/type_failures/**/*.ex") do
     test "type check failure case #{file}" do
       result = capture_io(fn -> ExType.check(unquote(file)) end)
-      refute String.contains?(result, [Emoji.one_test_pass()])
-      assert String.contains?(result, [Emoji.one_test_fail()])
+
+      refute String.contains?(result, [Emoji.one_test_pass()]), result
+      assert String.contains?(result, [Emoji.one_test_fail()]), result
     end
   end
 end
